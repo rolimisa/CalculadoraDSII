4ce337101bba706746a795e4714f2ee93014976d
LabInfo <LabInfo@TERREOA3M11>
2025-03-19 21:23:46 -0300

    Adicionar arquivos de projeto.

---
+++ CalculadoraSolution-master/Calculadora/Calculadora.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Calculadora
+    {
+        public double Somar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Subtrair(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        public double Dividir(double num1, double num2)
+        {
+            if (num2 != 0)
+            {
+                return num1 / num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("Erro: Divisão por zero não permitida.");
+            }
+        }
+    }
+}
---
+++ CalculadoraSolution-master/Calculadora/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double num1, num2, resultado;
+        string operacao;
+
+        Console.WriteLine("Calculadora - Operações Básicas");
+
+        Console.Write("Digite o primeiro número: ");
+        num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a operação (+, -, *, /): ");
+        operacao = Console.ReadLine();
+
+        Console.Write("Digite o segundo número: ");
+        num2 = Convert.ToDouble(Console.ReadLine());
+
+        switch (operacao)
+        {
+            case "+":
+                resultado = num1 + num2;
+                Console.WriteLine($"Resultado: {num1} + {num2} = {resultado}");
+                break;
+
+            case "-":
+                resultado = num1 - num2;
+                Console.WriteLine($"Resultado: {num1} - {num2} = {resultado}");
+                break;
+
+            case "*":
+                resultado = num1 * num2;
+                Console.WriteLine($"Resultado: {num1} * {num2} = {resultado}");
+                break;
+
+            case "/":
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                    Console.WriteLine($"Resultado: {num1} / {num2} = {resultado}");
+                }
+                else
+                {
+                    Console.WriteLine("Erro: Divisão por zero não permitida.");
+                }
+                break;
+
+            default:
+                Console.WriteLine("Operação inválida! Tente novamente com +, -, * ou /.");
+                break;
+        }
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
